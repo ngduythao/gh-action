44c94030deba14323dd6918cda6c9c42972c7073
ngduythao <ngduythao2805@gmail.com>
2022-07-03 10:33:26 +0700

    Goodbye world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
   }
 }
