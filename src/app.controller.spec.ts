c2774ab795b20bb390ef1fc449416cc6910fe8c5
ngduythao <ngduythao2805@gmail.com>
2022-07-29 23:04:16 +0700

    Revert

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Goodbye World!!"', () => {
-      expect(appController.getGoodbye()).toBe('Goodbye World!!');
+    it('should return "Goodbye World!"', () => {
+      expect(appController.getGoodbye()).toBe('Goodbye World!');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getGoodbye(): string {
-    return 'Goodbye World!!';
+    return 'Goodbye World!';
   }
 }
